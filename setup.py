82b5a0380bf76fec7d7293c303c00cad19d73d40
chrissienator <christian-schreurs@live.nl>
2022-12-27 20:11:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,16 @@ install_requires = [
         'matplotlib>=2.2.3',
     ]
 
+"""
 here = os.path.abspath(os.path.dirname(__file__))
 _version = {}
 _version_path = os.path.join(here, 'SyntheticControlMethod', '__version__.py')
 with open(_version_path, 'r', 'utf-8') as f:
     exec(f.read(), _version)
+"""
     
 setup(
     name='SyntheticControlMethod',
-    version=_version['__version__'],
     author='Christian Schreurs',
     author_email='christian-schreurs@live.nl',
     url='https://github.com/OscarEngelbrektson/SyntheticControlMethods',
