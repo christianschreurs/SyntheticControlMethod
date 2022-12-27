e452d939e4e5de8cc6401b8f67e8cfe8288a8d8e
chrissienator <christian-schreurs@live.nl>
2022-12-27 19:27:31 +0100

    Create setup.py
    Create setup python file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='SyntheticControlMethod',
+    version='0.0.1',
+    author='Christian Schreurs',
+    author_email='christian-schreurs@live.nl',
+    url='https://github.com/OscarEngelbrektson/SyntheticControlMethods',
+    download_url='https://github.com/christianschreurs/synthetic-control-method',
+    description= "A Python package for Synthetic Control Methodology",
+)
