1d1628c85c08f93a964d617973700046735362fe
chrissienator <christian-schreurs@live.nl>
2022-12-27 19:42:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
+import os
 
+
+#Get version
+here = os.path.abspath(os.path.dirname(__file__))
+_version = {}
+_version_path = os.path.join(here, 'SyntheticControlMethod', '__version__.py')
+with open(_version_path, 'r', 'utf-8') as f:
+    exec(f.read(), _version)
+    
 setup(
     name='SyntheticControlMethod',
-    version='0.0.1',
+    version=_version['__version__'],
     author='Christian Schreurs',
     author_email='christian-schreurs@live.nl',
     url='https://github.com/OscarEngelbrektson/SyntheticControlMethods',
     download_url='https://github.com/christianschreurs/synthetic-control-method',
     description= "A Python package for Synthetic Control Methodology",
 )
+
+
